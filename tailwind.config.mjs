11b65c4bed9665bed4df58e82a7a47b48ad6ca6b
vandaminator <ntokoanephomolo76@gmail.com>
2024-06-14 12:34:52 +0200

    Remove daisyui

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,3 @@
-import daisyui from "daisyui"
-
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -13,26 +11,5 @@ export default {
 				'accent': 'var(--accent)',
 			},
 		},
-	},
-	plugins: [daisyui],
-	daisyui: {
-		themes: [
-			{
-				light: {
-					"primary": "#312dd6",
-					"secondary": "#8987f3",
-					"accent": "#514dfc",
-					"neutral": "#170f3d",
-					"base-100": "#f8f7fd",
-				},
-				dark: {
-					"primary": "#2c29d1",
-					"secondary": "#0e0c79",
-					"accent": "#0603b0",
-					"neutral": "#170f3d",
-					"base-100": "#030208",
-				},
-			},
-		],
 	},
 }
