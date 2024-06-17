708df1ea8002b25195724abe748d13171a29fe7c
vandaminator <ntokoanephomolo76@gmail.com>
2024-06-17 18:01:21 +0200

    Added Dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        text: "hsl(var(--text))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
