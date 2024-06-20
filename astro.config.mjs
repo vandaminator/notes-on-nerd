1914390ec330a0346e6680e4fe1f02d33145e97c
vandaminator <ntokoanephomolo76@gmail.com>
2024-06-20 12:46:26 +0200

    Added site on astro.config for opengraph

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from 'astro/config';
+
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-
 import react from "@astrojs/react";
+
+
+const siteName = import.meta.env.SITENAME ?? "https://notes-on-nerd.vercel.app/"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: siteName,
   integrations: [mdx(), sitemap(), react(), tailwind({
     applyBaseStyles: false,
   }),],
